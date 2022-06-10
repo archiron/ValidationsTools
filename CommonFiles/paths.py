74b6e47e018275683e6f215413be089c65ec12c3
archiron <arnaud.chiron@cern.ch>
2022-06-10 15:04:34 +0200

    Version 2022.06.10-01
    add a "paths" file with all paths needed for saves or works. This is different from "sources" file which have only files (such as ROOT files) needed for the computation.
    remove one unused file (.pyc).

---
+++ CommonFiles/paths.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+#-*-coding: utf-8 -*-
+
+################################################################################
+# sources.py : list of ROOT files to be used with zeeExtract tools
+# for egamma validation comparison                              
+# 
+# MUST be launched with the cmsenv cmd after a cmsrel cmd !!
+#                                                                              
+# Arnaud Chiron-Turlay LLR - arnaud.chiron@llr.in2p3.fr                        
+#                                                                              
+################################################################################
+
+# CCA
+LOG_SOURCE='/pbs/home/c/chiron/private/ZEE_Flow/CMSSW_12_1_0_pre5/src/Kolmogorov'
+LOG_OUTPUT='/sps/cms/chiron/TEMP/'
+RESULTFOLDER='/sps/cms/chiron/CMSSW_12_1_0_pre5-16c-10'
+
+# POLUI
+LOG_SOURCE="/home/llr/info/chiron_u/PYTHON/ZEE_FLOW/CMSSW_11_0_0_pre13/src"
+LOG_OUTPUT="/home/llr/info/chiron_u/PYTHON/ZEE_FLOW/CMSSW_11_0_0_pre13/src/TEMP"
+RESULTFOLDER="/data_CMS/cms/chiron/HGCAL/TEST_12_1_0_pre5bb"
