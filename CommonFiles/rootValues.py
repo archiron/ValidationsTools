5f9fcb7b6da1d3c0f0e1dbfc09b914833f9068c8
archiron <arnaud.chiron@cern.ch>
2022-07-08 11:33:38 +0200

    Version 2022.07.08-03
    small change into rootValues.py

--- CommonFiles/rootValues.py
+++ CommonFiles/rootValues.py
@@ -6,6 +6,8 @@
 # this help for the creation of a ROOT file with NB_EVTS events from number
 # Nbegin to number Nend
 # for egamma validation comparison                              
+# NOTE : Nbegin..Nend are the numbers of each ROOT file and 
+# NOT the number of events inside the NB_EVTS events.
 # 
 # Arnaud Chiron-Turlay LLR - arnaud.chiron@llr.in2p3.fr                        
 #                                                                              
