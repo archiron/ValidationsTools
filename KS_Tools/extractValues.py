fea7c77d58cdc10584754758d3a83f32577adc3e
archiron <arnaud.chiron@cern.ch>
2022-10-10 17:32:22 +0200

    Version 2022.10.10-01
    take the absence of histo into account and avoid some crashes.

--- KS_Tools/extractValues.py
+++ KS_Tools/extractValues.py
@@ -95,62 +95,68 @@ for elem in fileList:
         print("== %s ==" % leaf)
         temp_leaf = []
         histo = h1.Get(leaf)
-        d = getHistoConfEntry(histo)
+        if (histo):
+            print('%s OK' % leaf)
+            d = getHistoConfEntry(histo)
 
-        temp_leaf.append(histo.GetMean()) # 0
-        temp_leaf.append(histo.GetMeanError()) # 2
-        temp_leaf.append(histo.GetStdDev()) # 6
-        temp_leaf.append(histo.GetEntries()) # 6b
-
-        temp_leaf.append(name_1) # 7
-        #print('temp_leaf : %s' % temp_leaf)
+            temp_leaf.append(histo.GetMean()) # 0
+            temp_leaf.append(histo.GetMeanError()) # 2
+            temp_leaf.append(histo.GetStdDev()) # 6
+            temp_leaf.append(histo.GetEntries()) # 6b
             
-        texttoWrite = ''
-        i=0
-        for entry in histo:
-            #print(i,entry)
-            texttoWrite += 'b_' + '{:03d}'.format(i) + ',c_' + '{:03d},'.format(i)
-            if ( d == 1):
-                temp_leaf.append(entry) # b_
-            else :
-                if ((histo.GetBinEntries(i) == 0.) and (entry == 0.)):
-                    temp_leaf.append(0.)
-                elif ((histo.GetBinEntries(i) == 0.) and (entry != 0.)):
-                    temp_leaf.append(1.e38)
-                    print('========================================',i,entry,histo.GetBinEntries(i))
-                else:
-                    temp_leaf.append(entry/histo.GetBinEntries(i))
-            temp_leaf.append(histo.GetBinError(i)) # c_
-            i+=1
-        print('there is %d entries' % i)
-        texttoWrite = texttoWrite[:-1] # remove last char
-        temp_leaf.append(texttoWrite) # end
-        histos[leaf].append(temp_leaf)
+            temp_leaf.append(name_1) # 7
+            #print('temp_leaf : %s' % temp_leaf)
+                
+            texttoWrite = ''
+            i=0
+            for entry in histo:
+                #print(i,entry)
+                texttoWrite += 'b_' + '{:03d}'.format(i) + ',c_' + '{:03d},'.format(i)
+                if ( d == 1):
+                    temp_leaf.append(entry) # b_
+                else :
+                    if ((histo.GetBinEntries(i) == 0.) and (entry == 0.)):
+                        temp_leaf.append(0.)
+                    elif ((histo.GetBinEntries(i) == 0.) and (entry != 0.)):
+                        temp_leaf.append(1.e38)
+                        print('========================================',i,entry,histo.GetBinEntries(i))
+                    else:
+                        temp_leaf.append(entry/histo.GetBinEntries(i))
+                temp_leaf.append(histo.GetBinError(i)) # c_
+                i+=1
+            print('there is %d entries' % i)
+            texttoWrite = texttoWrite[:-1] # remove last char
+            temp_leaf.append(texttoWrite) # end
+            histos[leaf].append(temp_leaf)
+        else:
+            print('%s KO' % leaf)
+
 
 #print histos into histo named files
 i_leaf = 0
 for leaf in branches:
-    fileName = resultPath + 'histo_' + str(leaf) + '_' + '{:03d}'.format(nbFiles) + '.txt'
-    print('fileName : %s' % fileName)
-    wr.append(open(fileName, 'w'))
-    nb_max = len(histos[leaf][0]) - 1
-    print("== %s == nb_max : %d" % (leaf, nb_max))
-    wr[i_leaf].write('evol,Mean,MeanError,StdDev,nbBins,name,')
-    wr[i_leaf].write(str(histos[leaf][0][nb_max]))
-    wr[i_leaf].write('\n')
-    
-    for i_file in range(0, len(fileList)):
-        texttoWrite = str(i_file) + ','
-        wr[i_leaf].write(texttoWrite) 
-        for i in range(0, nb_max-1):
-            #print('i : %d' % i)
-            wr[i_leaf].write(str(histos[leaf][i_file][i]))
-            wr[i_leaf].write(',')
-        wr[i_leaf].write(str(histos[leaf][i_file][nb_max-1]))
-        texttoWrite = '\n'
-        wr[i_leaf].write(texttoWrite) 
-    wr[i_leaf].close()
-    i_leaf +=1
+    histo = h1.Get(leaf)
+    if (histo):
+        fileName = resultPath + 'histo_' + str(leaf) + '_' + '{:03d}'.format(nbFiles) + '.txt'
+        print('fileName : %s' % fileName)
+        wr.append(open(fileName, 'w'))
+        nb_max = len(histos[leaf][0]) - 1
+        print("== %s == nb_max : %d" % (leaf, nb_max))
+        wr[i_leaf].write('evol,Mean,MeanError,StdDev,nbBins,name,')
+        wr[i_leaf].write(str(histos[leaf][0][nb_max]))
+        wr[i_leaf].write('\n')
+        
+        for i_file in range(0, len(fileList)):
+            texttoWrite = str(i_file) + ','
+            wr[i_leaf].write(texttoWrite) 
+            for i in range(0, nb_max-1):
+                wr[i_leaf].write(str(histos[leaf][i_file][i]))
+                wr[i_leaf].write(',')
+            wr[i_leaf].write(str(histos[leaf][i_file][nb_max-1]))
+            texttoWrite = '\n'
+            wr[i_leaf].write(texttoWrite) 
+        wr[i_leaf].close()
+        i_leaf +=1
 
 print("Fin !")
 
